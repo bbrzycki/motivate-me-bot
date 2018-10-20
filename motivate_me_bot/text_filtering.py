abf65307d04f6e965a5fa382670a165bfea10c07
Bryan Brzycki <bbrzycki@college.harvard.edu>
2018-10-20 13:55:16 -0700

    Add some basic punctuation and capitalization handling.

--- motivate_me_bot/text_filtering.py
+++ motivate_me_bot/text_filtering.py
@@ -2,13 +2,31 @@ from screen_tweets import *
 
 import sys
 import os
+import regex
 from autocorrect import spell
 
 def filter_quote(full_text, autocorrect=False):
     '''Filter the tweet to be more presentable as a standalone quote'''
-    # TODO: If a new line does *not* have punctuation, then add it..
-    # Otherwise don't add anything
-    word_list = full_text.replace('\n', ' ').split()
+    # If a new line does *not* have punctuation, then add it,
+    # otherwise don't add anything. Match using regex.
+    lines = full_text.split('\n')
+    lines[:] = [line for line in lines if line != '']
+
+    punctuation = regex.compile(r'[\p{C}|\p{M}|\p{P}|\p{S}|\p{Z}]+', regex.UNICODE)
+    for i in range(len(lines)):
+        if i == 0:
+            lines[i] = lines[i][0].upper() + lines[i][1:]
+        if punctuation.match(lines[i].strip()[-1]) is None:
+            if i == len(lines) - 1:
+                lines[i] = lines[i].strip() + '.'
+            elif lines[i + 1].strip()[0].islower():
+                lines[i] = lines[i].strip() + ','
+            else:
+                lines[i] = lines[i].strip() + '.'
+        else:
+            if i < len(lines) - 1:
+                lines[i + 1] = lines[i + 1][0].upper() + lines[i + 1][1:]
+    word_list = ' '.join(lines).split()
     print('-'*20)
     print(full_text)
     print(word_list)
